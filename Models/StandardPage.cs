0a1b8de54029e955a8e1ddc14eb131d257815909
Jojo Aquino <jfaquinojr@gmail.com>
2018-11-10 08:11:06 +0800

    scaffold piranhacms

---
+++ Models/StandardPage.cs
@@ -0,0 +1,11 @@
+using Piranha.AttributeBuilder;
+using Piranha.Extend.Fields;
+using Piranha.Models;
+
+namespace batch97.Models
+{
+    [PageType(Title = "Standard page")]
+    public class StandardPage  : Page<StandardPage>
+    {
+    }
+}
